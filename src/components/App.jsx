b8f208baa8dcb44ac3b8b2ef411f1d6689f123ee
angeliquemari <angelique@nehmzow.com>
2019-09-17 11:52:19 -0700

    link video entry list to video player, add state and onClick handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,32 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {data: exampleVideoData, selected: exampleVideoData[0]};
+    this.state = {data: exampleVideoData, selected: exampleVideoData[0]};
   }
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            ` <div><h5><em>search</em> view goes here</h5></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
-            <div><VideoPlayer video={exampleVideoData[0]} /></div>
-            {/* <div><VideoPlayer video={this.state} /></div> */}
+            <div><VideoPlayer video={this.state.selected} /></div>
           </div>
           <div className="col-md-5">
-            {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
-            <div><VideoList videos={exampleVideoData} /></div>
-            {/* <div><VideoList videos={this.state} /></div> */}
-            {/* onClick={this.onVideoEntryClick}  */}
+            <div><VideoList videos={this.state.data} onClick={this.onVideoEntryClick.bind(this)} /></div>
           </div>
         </div>
       </div>
     );
   }
-  // onVideoEntryClick() {
-  //   this.setState({
-  //     selected: // clicked video
-  //   })
-  // }
+  onVideoEntryClick(video) {
+    this.setState({
+      selected: video
+    });
+  }
 }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,13 +2,10 @@ var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId} allowFullScreen></iframe>
-      {/* props.state.selected.id.videoId */}
     </div>
     <div className="video-player-details">
       <h3>{props.video.snippet.title}</h3>
-      {/* <h3>{props.state.selected.snippet.title}</h3> */}
       <div>{props.video.snippet.description}</div>
-      {/* <div>{props.state.selected.snippet.description}</div> */}
     </div>
   </div>
 );
