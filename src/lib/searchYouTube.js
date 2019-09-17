f079591b865216ba0675e267f6840d122885f6e9
angeliquemari <angelique@nehmzow.com>
2019-09-17 16:21:27 -0700

    add helper function to make youtube api call

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      type: 'video',
+      videoEmbeddable: 'true'
+    },
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Successfully got videos');
+      console.log(data.items);
+      callback(data.items);
+    },
+    error: function (data) {
+      console.error('Failed to get videos', data);
+    }
+  });
 };
 
 export default searchYouTube;
